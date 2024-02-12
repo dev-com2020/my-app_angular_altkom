7e2b5a0613788119558fd666c754998411e9d60b
DEV-COM <kwp@tlen.pl>
2024-02-12 15:56:17 +0100

    15:56

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -4,6 +4,8 @@ import {
   ChangeDetectionStrategy, OnInit, OnChanges, SimpleChange,
   SimpleChanges
 } from '@angular/core';
+import { Product } from '../product';
+
 
 @Component({
   selector: 'app-product-detail',
@@ -16,33 +18,33 @@ import {
 })
 export class ProductDetailComponent implements OnInit, OnChanges {
 
-  @Input() name = ''
+  @Input() product: Product | undefined;
   @Output() bought = new EventEmitter<string>();
 
   constructor() {
-    console.log(`Name is ${this.name} in the constructor`)
+    console.log(`Name is ${this.product?.name} in the constructor`)
   }
 
   ngOnInit(): void {
-    console.log(`Name is ${this.name} in the ngOnInit`)
+    console.log(`Name is ${this.product?.name} in the ngOnInit`)
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const product = changes['name']
+    const product = changes['product']
     if (!product.isFirstChange()) {
-      const oldValue = product.previousValue
-      const newValue = product.currentValue
+      const oldValue = product.previousValue.name
+      const newValue = product.currentValue.name
       console.log(`Product change from ${oldValue} to ${newValue}`)
     }
   }
 
   buy() {
-    this.bought.emit(this.name);
+    this.bought.emit(this.product?.name);
   }
 
-  get productName(): string {
-    console.log(`Get ${this.name}`)
-    return this.name
+  get productName(): any {
+    console.log(`Get ${this.product?.name}`)
+    return this.product?.name
   }
 }
 
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { CommonModule } from '@angular/common';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-list',
@@ -10,10 +11,24 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  selectedProduct = ''
-  products = ['Webcam', 'Microphone', 'Keyboard']
+  today = new Date();
+  selectedProduct: Product | undefined
+  products: Product[] = [
+    {
+      name: 'Webcam',
+      price: 100,
+    },
+    {
+      name: 'Microphone',
+      price: 200,
+    },
+    {
+      name: 'Keyboard',
+      price: 85
+    }
+  ]
 
-  onBuy(name: string){
+  onBuy(name: string) {
     window.alert(`Zakupiłeś ${name}`)
   }
 }
