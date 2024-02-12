f0711ed3c7257f91ee636bda3846c681f09548cb
DEV-COM <kwp@tlen.pl>
2024-02-12 13:24:48 +0100

    13:24 12.02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './products/product-list/product-list.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Altkom';
+  opis2 = 'kurs Angular';
 }
+
+// type Animal = 'Żyrafa' | "Słoń";
+// enum Marki {"a","b","c"}
+// let mojaMarka: Marki.a
+// let mojaMarka1: string = Marki[0]
+
+// let an : Animal = 'Słoń';
+
+// function hello(name: string = 'Tomek', greet?:string, ...names:string[]): string {
+//   if (!greet){
+//     greet = 'Witaj'
+//   }
+//   return 'Hello ' + name
+// }
+
+// let sayHello: (name: string) => string = function(name:string): string {
+//   return 'Hello ' + name
+// }
+
+// let url = `${baseUrl}/${path}?param=${param}`
---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [ProductDetailComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  selectedProduct = ''
+}
