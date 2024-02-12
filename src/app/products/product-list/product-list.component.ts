1bd3eb607a1afac02d3b233575010a316e66c2e1
DEV-COM <kwp@tlen.pl>
2024-02-12 14:47:03 +0100

    14:46

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [ProductDetailComponent],
+  imports: [ProductDetailComponent, CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  selectedProduct = 'Keyboard'
+  selectedProduct = ''
   onBuy(name: string){
     window.alert(`Zakupiłeś ${name}`)
   }
