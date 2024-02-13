5d6160617a687aca5d5ee43eae784cbdf5734196
DEV-COM <kwp@tlen.pl>
2024-02-13 14:18:46 +0100

    14:18 13.02

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { CommonModule } from '@angular/common';
 import { Product } from '../product';
@@ -6,6 +6,7 @@ import { SortPipe } from '../sort.pipe';
 import { ProductsService } from '../products.service';
 import { FavoritesComponent } from '../favorites/favorites.component';
 import { ProductViewComponent } from '../product-view/product-view.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -16,15 +17,16 @@ import { ProductViewComponent } from '../product-view/product-view.component';
   styleUrl: './product-list.component.css',
   providers: [ProductsService]
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   today = new Date();
+  private productsSub: Subscription | undefined
   selectedProduct: Product | undefined
   products: Product[] = []
   
   constructor(private productService: ProductsService){}
 
   private getProducts(){
-    this.productService.getProducts().subscribe(products => {
+    this.productsSub = this.productService.getProducts().subscribe(products => {
       this.products = products
     })
   }
@@ -32,6 +34,11 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
       this.getProducts()
   }
+
+  ngOnDestroy(): void {
+      this.productsSub?.unsubscribe()
+  }
+  
 
   onBuy(name: string) {
     window.alert(`Zakupiłeś ${name}`)
--- src/app/products/product-view/product-view.component.ts
+++ src/app/products/product-view/product-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ProductViewService } from './product-view.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-product-view',
@@ -9,13 +10,16 @@ import { ProductViewService } from './product-view.service';
   styleUrl: './product-view.component.css',
   providers: [ProductViewService]
 })
-export class ProductViewComponent implements OnInit {
+export class ProductViewComponent implements OnInit, OnDestroy {
 @Input() id = -1
 name = ''
+private productSub = new Subject<void>
 constructor(private productViewService: ProductViewService) {}
 
 private getProduct() {
-  this.productViewService.getProduct(this.id).subscribe(product =>{
+  this.productViewService.getProduct(this.id).pipe(
+    takeUntil(this.productSub)
+  ).subscribe(product =>{
     if (product) {
       this.name = product.name
     }
@@ -25,5 +29,9 @@ private getProduct() {
 
 ngOnInit(): void {
   this.getProduct()
+}
+ngOnDestroy(): void {
+    this.productSub.next()
+    this.productSub.complete()
 }
 }
