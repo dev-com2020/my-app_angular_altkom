4d32dd1fcba0eb736c88b673d259520d02d8011e
DEV-COM <kwp@tlen.pl>
2024-02-14 14:13:28 +0100

    14:13 14.02

--- src/app/page-not-found/page-not-found.component.spec.ts
+++ src/app/page-not-found/page-not-found.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import { By } from '@angular/platform-browser';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
@@ -20,4 +21,25 @@ describe('PageNotFoundComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render oops!', () => {
+    const fixture = TestBed.createComponent(PageNotFoundComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('p')?.textContent).toContain('Ooops!');
+  });
+
+  it('should call goHome method when click button', () => {
+    spyOn(component, 'goHome')
+    let button = fixture.debugElement.nativeElement.querySelector('button')
+    button.click()
+    expect(component.goHome).toHaveBeenCalled()
+  });
+
+  it('should call goHome at css when click button', () => {
+    spyOn(component, 'goHome')
+    let button = fixture.debugElement.query(By.css('button'))
+    button.triggerEventHandler('click')
+    expect(component.goHome).toHaveBeenCalled()
+  })
 });
